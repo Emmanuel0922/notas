537bcfd670cecac362b1713a7ecf57d53af23daa
Emmanuel0922 <92409192+Emmanuel0922@users.noreply.github.com>
2023-05-21 22:27:12 -0400

    cambio5

--- lib/src/ui/pages/home/perfil/perfil_page.dart
+++ lib/src/ui/pages/home/perfil/perfil_page.dart
@@ -9,6 +9,39 @@ class PerfilPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Container(
+        color: Colors.blue, // Color de fondo azul
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                  radius: 75,
+                  backgroundImage: NetworkImage(
+                      'https://static.vecteezy.com/system/resources/previews/014/296/309/non_2x/blue-verified-social-media-account-icon-approved-profile-sign-illustration-vector.jpg')),
+              SizedBox(height: 16.0),
+              Text(
+                'Nombre de Usuario',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                'Descripción del perfil',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 16.0),
+            ],
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(
           Icons.power_settings_new,
--- lib/src/ui/pages/register/register_controller.dart
+++ lib/src/ui/pages/register/register_controller.dart
@@ -1,12 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:notas/src/models/dto/user_dto.dart';
 import 'package:notas/src/models/entity/usuarios_model.dart';
 import 'package:notas/src/models/store.dart';
 // ignore: depend_on_referenced_packages
 import 'package:crypto/crypto.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class RegisterController extends GetxController {
   TextEditingController nombreText = TextEditingController();
@@ -16,7 +14,7 @@ class RegisterController extends GetxController {
   Future<void> registro() async {
     String name = nombreText.text;
     String email = correoText.text.trim().toLowerCase();
-    String clave = claveText.text.trim();
+    String clave = correoText.text.trim();
 
     if (name.isEmpty) {
       Get.snackbar('Error', 'El Nombre es inválido');
@@ -30,11 +28,12 @@ class RegisterController extends GetxController {
       Get.snackbar('Error', 'La clave es inválida');
       return;
     }
-    // else if (!RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
-    //     .hasMatch(clave)) {
-    //   Get.snackbar('Error', 'La contraseña no cumple los requisitos');
-    //   return;
-    // }
+    RegExp claveValida = RegExp(r'^.{8,}$');
+
+    if (!claveValida.hasMatch(clave)) {
+      Get.snackbar('Error', 'La clave no cumple con los requisitos');
+      return;
+    }
 
     // verificar si el usuario ya está registrado
     Users? usuarioExistente = await Database.getUserByEmail(email);
@@ -51,12 +50,6 @@ class RegisterController extends GetxController {
 
     // Guarda el nuevo usuario en el store de usuarios
     Database.userBox.put(user);
-
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    UserDto userDto =
-        UserDto(id: user.id, name: user.name, email: user.email, pass: '');
-    prefs.setString('usuario', userDto.toJson().toString());
-    Get.offAll('/home');
 
     // Muestra una notificación de éxito
     Get.snackbar('Éxito', 'El usuario ha sido registrado exitosamente');
