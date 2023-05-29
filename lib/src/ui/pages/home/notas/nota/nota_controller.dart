076a0b441a5d832e398b28ab47f55e220c0906ab
Emmanuel0922 <92409192+Emmanuel0922@users.noreply.github.com>
2023-05-28 22:48:24 -0400

    pagina de notas agregada1

--- lib/src/ui/pages/home/notas/nota/nota_controller.dart
+++ lib/src/ui/pages/home/notas/nota/nota_controller.dart
@@ -2,8 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class NotasController extends GetxController {
-  
+  final TextEditingController noteController = TextEditingController();
+  final RxList<String> notes = <String>[].obs;
+
   navNuavaNota() {
     Get.toNamed('/nueva_nota');
+  }
+
+  void addNote() {
+    String newNote = noteController.text.trim();
+    if (newNote.isNotEmpty) {
+      notes.add(newNote);
+      update();
+    }
+    Get.back();
+  }
+
+  void editNoteAt(int index) {
+    String updatedNote = noteController.text.trim();
+    if (updatedNote.isNotEmpty) {
+      notes[index] = updatedNote;
+      update();
+    }
+    Get.back();
   }
 }
--- lib/src/ui/pages/home/notas/nota/nota_page.dart
+++ lib/src/ui/pages/home/notas/nota/nota_page.dart
@@ -3,19 +3,180 @@ import 'package:get/get.dart';
 import 'package:notas/src/ui/pages/home/notas/nota/nota_controller.dart';
 import 'package:notas/src/ui/widgets/widgets.dart';
 
-class NotasPage extends StatelessWidget {
-  NotasController con = Get.put(NotasController());
+// class NotasPage extends StatelessWidget {
+//   NotasController con = Get.put(NotasController());
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       floatingActionButton: FloatingActionButton(
+//         child: const Icon(
+//           Icons.add,
+//           color: Colors.green,
+//         ),
+//         onPressed: () => con.navNuavaNota(),
+//       ),
+
+//     );
+//   }
+// }
+
+class NotasPage extends StatefulWidget {
+  @override
+  _NotesPageState createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotasPage> {
+  var not1 = Get.put(NotasController());
+
+  @override
+  void dispose() {
+    not1.noteController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(
-          Icons.add,
-          color: Colors.green,
-        ),
-        onPressed: () => con.navNuavaNota(),
+      appBar: AppBar(
+        title: Text('My Notes'),
       ),
+      body: not1.notes.isEmpty
+          ? Center(
+              child: Text(
+                'No notes added',
+                style: TextStyle(fontSize: 16),
+              ),
+            )
+          : ListView.builder(
+              itemCount: not1.notes.length,
+              itemBuilder: (context, index) {
+                return Obx(
+                  () => Card(
+                    color: Colors.blue[50],
+                    elevation: 2.0,
+                    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      side: BorderSide(color: Colors.blue),
+                    ),
+                    child: ListTile(
+                      title: Text(
+                        not1.notes[index],
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.blue[900],
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.edit, color: Colors.blue),
+                            onPressed: () {
+                              _showEditNoteDialog(context, index);
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete, color: Colors.blue),
+                            onPressed: () {
+                              setState(() {
+                                not1.notes.removeAt(index);
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          _showAddNoteDialog(context);
+        },
+      ),
+    );
+  }
+
+  void _showAddNoteDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        not1.noteController.clear();
+        return AlertDialog(
+          title: Text('Add a Note'),
+          content: TextField(
+            controller: not1.noteController,
+            decoration: InputDecoration(
+              hintText: 'Enter your note',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            ElevatedButton(
+              child: Text('Add'),
+              onPressed: () {
+                String newNote = not1.noteController.text.trim();
+                if (newNote.isNotEmpty) {
+                  setState(() {
+                    not1.notes.add(newNote);
+                  });
+                }
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showEditNoteDialog(BuildContext context, int index) {
+    not1.noteController.text = not1.notes[index];
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Edit Note'),
+          content: TextField(
+            controller: not1.noteController,
+            decoration: InputDecoration(
+              hintText: 'Enter your note',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            ElevatedButton(
+              child: Text('Update'),
+              onPressed: () {
+                String updatedNote = not1.noteController.text.trim();
+                if (updatedNote.isNotEmpty) {
+                  setState(() {
+                    not1.notes[index] = updatedNote;
+                  });
+                }
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
